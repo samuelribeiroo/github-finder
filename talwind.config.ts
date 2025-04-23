7784f41907cff42d752aa5ad915c7b1cc84c6ac1
samuelribeiroo <samuelribeirodev@gmail.com>
2025-04-23 11:17:16 -0300

    initial commit

---
+++ talwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-nunito-sans)"],
+      },
+      colors: {},
+    },
+  },
+  plugins: [],
+}
+
+export default config

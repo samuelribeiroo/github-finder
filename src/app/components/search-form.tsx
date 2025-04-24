6124693d0847a7ae1b9f82a025a6b55006a54c3d
samuelribeiroo <samuelribeirodev@gmail.com>
2025-04-24 17:53:14 -0300

    feat: implement user search

--- src/app/components/search-form.tsx
+++ src/app/components/search-form.tsx
@@ -1,15 +1,51 @@
+'use client'
+
+import { LoaderIcon } from "lucide-react";
 import Button from "../ui/button";
 import Input from "../ui/input";
+import useGithubSearch from "@/hooks/useGithubSearch";
+import UserProfile from "./user-profile";
+import NotFound from "./not-found";
 
 export default function SearchForm() {
+  const { searchTerm, setsearchTerm, user, isLoading, error } =
+    useGithubSearch();
+
   return (
     <>
-      <form className="w-full max-w-[503px]">
+      <form
+        className="w-full max-w-[503px]"
+        onSubmit={(event) => event.preventDefault()}
+      >
         <div className="flex overflow-hidden rounded-lg">
-          <Input />
+          <Input
+            type="text"
+            value={searchTerm}
+            onChange={(event: any) => setsearchTerm(event.target.value)}
+          />
           <Button />
         </div>
       </form>
+
+      {
+      <section className="">
+        <div className="">
+          {isLoading && (
+            <div className="flex justify-center mt-8">
+              <LoaderIcon />
+            </div>
+          )}
+
+          {error && <NotFound /> }
+
+          {!isLoading && !error && user && <UserProfile user={user} />}
+
+          {!isLoading && !error && !user && searchTerm && (
+           <NotFound />
+          )}
+        </div>
+      </section>
+      }
     </>
   );
 }
--- src/app/components/user-profile.tsx
+++ src/app/components/user-profile.tsx
@@ -1,9 +1,35 @@
-export default function UserProfile() {
+import Image from "next/image";
+import { GitHubUser } from "../@types/index";
+
+type GitHubProps = {
+  user: GitHubUser 
+}
+
+export default function UserProfile({ user }: GitHubProps) {
   return (
     <>
-     <div className="user-profile-container">
-      
-     </div>
+      <section className="general-container">
+        <div className="flex flex-col items-center gap-6 md:flex-row md:items-start">
+          <div className="relative size-40 flex-shrink-0 overflow-hidden rounded-full border-4 border-[#005CFF] md:h-48 md:w-48">
+            <Image
+              src={user.avatar_url}
+              alt={`Foto de perfil do usuário ${user.name}`}
+              fill
+              className="object-cover"
+              sizes="(max-width: 768px) 220px, 220px"
+              fetchPriority="high"
+            />
+          </div>
+
+          <div className="flex flex-col mt-10 text-center md:text-left gap-4 max-w-2xl">
+            <h1 className="text-[#005CFF] text-xl font-bold">{user.name}</h1>
+
+            <p className="text-black font-light text-base leading-relaxed">
+            {user.bio}
+            </p>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
